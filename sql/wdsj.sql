960aaa488a5f3ccec417750b92eecab99415557a
root <root@localhost.localdomain>
2014-01-20 11:53:40 +0900

    Fixed

--- sql/wdsj.sql
+++ sql/wdsj.sql
@@ -1,3 +1,25 @@
+CREATE TABLE xml_type(
+       xml_type_id INTEGER unique not null,
+       xml_type CHAR(255) unique not null,
+       PRIMARY KEY (number)
+);
+
+INSERT INTO xml_type(1,'IUGONET');
+INSERT INTO xml_type(2,'JaLC');
+INSERT INTO xml_type(3,'XHTML');
+
+--
+
+CREATE TABLE xml(
+       doi_suffix CHAR(255) UNIQUE NOT NULL,	
+       version INTEGER NOT NULL,
+       user_id INTEGER NOT NULL,
+       xml_type_id INTEGER NOT NULL,
+       metadata TEXT NOT NULL,
+       PRIMARY KEY (doi_suffix, version, user_id, xml_type_id),
+       FOREIGN KEY (xml_type_id) REFERENCES xml_type(xml_type_id)
+);
+
 CREATE TABLE iugonet(
        doi_suffix CHAR(255) UNIQUE NOT NULL,
        version INTEGER NOT NULL,
@@ -8,7 +30,6 @@ CREATE TABLE iugonet(
 
 INSERT INTO iugonet VALUE('A1',1,'C1','D1');
 INSERT INTO iugonet VALUE('A2',2,'C2','D2');
-INSERT INTO iugonet VALUE('A3',3,'C3','D3');
 
 --
 
@@ -23,7 +44,6 @@ CREATE TABLE jalc(
 
 INSERT INTO jalc VALUE('E1',1,'G1','H1');
 INSERT INTO jalc VALUE('E2',2,'G2','H2');
-INSERT INTO jalc VALUE('E3',3,'G3','H3');
 
 --
 
@@ -37,15 +57,5 @@ CREATE TABLE html(
 
 INSERT INTO html VALUE('I1',1,'K1','L1');
 INSERT INTO html VALUE('I2',2,'K2','L2');
-INSERT INTO html VALUE('I3',3,'K3','L3');
 
 --
-
--- CREATE TRIGGER trigger_iugonet_insert AFTER INSERT ON tbl_name FOR EACH ROW trigger_stmt
--- CREATE TRIGGER trigger_iugonet_update AFTER UPDATE ON tbl_name FOR EACH ROW trigger_stmt
--- CREATE TRIGGER trigger_iugonet_delete AFTER DELETE ON tbl_name FOR EACH ROW trigger_stmt
-
---
-
--- CREATE FUNCTION iugonet2jalc 
--- CREATE FUNCTION iugonet2html 
